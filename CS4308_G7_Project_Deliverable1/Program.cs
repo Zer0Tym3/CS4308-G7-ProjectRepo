e94878cc96968715c8a08d71f15d9133a2d8d2d8
Zer0Tym3 <zchry.mrnng@gmail.com>
2023-09-21 20:16:43 -0400

    Initial Comments

--- CS4308_G7_Project_Deliverable1/Program.cs
+++ CS4308_G7_Project_Deliverable1/Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Group 7: Zachary Morning, Phillip Ngo, David Nguyen, Armando Ortiz
 
-Console.WriteLine("Test");
+/* Deliverable1 Task: Scanner and Tokenizer
+ *  //// Scanner \\\\\
+ * 1. Scans a File (.SCL Type)
+ * 2. Determines if file is even readable, and acts accordingly
+ * 3. Parses the File, Seperating Operators, Variables, Kewords, identifiers
+ * 4. Filters out any comments or empty space / lines
+ * 5. Tokenizes Each.
+ *  //// Token \\\\\
+ *  Video shows using OOP to make a Dict. to reference it all.
+ * 
+ * 
+ * Deeper Explination:
+ * Watch the Video duh
+ */
+
+
