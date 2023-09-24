efd37ac8d6e7d90eae7964b127fd898cdd6bc684
KhangNgo99 <khang.ngo9399@gmail.com>
2023-09-24 19:07:43 -0400

    Update Scanner.py

--- CS4308-G7-ProjectDeliverable1/Scanner.py
+++ CS4308-G7-ProjectDeliverable1/Scanner.py
@@ -12,6 +12,11 @@ class Scanner:
         self.token_list = []
         pass
 
+def remove_items(test_list, item):
+    res = [i for i in test_list if i != item]
+
+    return res    
+
 # filterAndLex attempted to open a file, lex and tokenize desired text, and filter out anything else.
 def filterAndLex(File_name):
     try:
@@ -115,13 +120,80 @@ def filterAndLex(File_name):
 
     loopCount = 0
     for line in lineList:
-        lineList[loopCount] 
+        lineList[loopCount] - remove_items(line, '')
+        loopCount += 1
+
+    loopCount = 0
+    for line in lineList:
+        if '\n' in line[len(line) - 1]:
+            modifiedStr = line[len(line) - 1]
+            modifiedStr = modifiedStr[:-1]
+            line[len(line) - 1] = modifiedStr
+            lineList[loopCount] = line
+        loopCount += 1
+
+    loopCount = 0
+    for line in lineList:
+        if '//' in line:
+            line = line[:line.index('//')]
+            lineList[loopCount] = line
+        loopCount += 1
+
+
+    currentLineNum = 0
+    while currentLineNum < len(lineList):
+        if lineList[currentLineNum][0] == '':
+            lineList.remove(lineList[currentLineNum])
+        else:
+            currentLineNum += 1
+
+    lineList = list(filter(None, lineList))
+
+    return lineList
+
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+    
+def Convert(a):
+    it = iter(a)
+    res_dct = dict(zip(it, it))
+    return res_dct
+
+def merge_dictionaries(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
+
+if __name__ == "__main__":
+    sysArgv = sys.argv
+
+    ItemList = filterAndLex(sysArgv[1])
+
+    finalTokenList = []
+    megaDict = {}
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 tA = False
 text = 'Import "scl.h"'
-"""
 start = text.index('"')
 end = text.index('"', start + 1)
 substring = '"' + text[start + 1:end] + '"'
@@ -135,12 +207,5 @@ except:
 print(sString1, ', ', substring)
 if tA == True:
     print(sList2)
-print(len(sList2))"""
+print(len(sList2))
 
-
-splitLocation = text.find('^')
-beforeStr = text[:splitLocation]
-afterStr = text[splitLocation:]
-secondSplitLocation = splitLocation + afterStr[1:].find('^') + 1
-strStatement = text[splitLocation:secondSplitLocation + 1]
-afterStr = text[secondSplitLocation + 1:]
