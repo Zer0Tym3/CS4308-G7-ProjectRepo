b72f688af36597abf6f2764eede65ae61b4f9593
Zer0Tym3 <zchry.mrnng@gmail.com>
2023-10-16 15:41:51 -0400

    Update parser.py

--- CS4308-G7-ProjectDeliverable2/parser.py
+++ CS4308-G7-ProjectDeliverable2/parser.py
@@ -1,3 +1,14 @@
+from Scanner import *
+from Token import *
+import json
+
 class Parser:
-    def complie(self):
-        print("test")
+    def compile(self):
+        print("Lorem Ipsum")
+    def getNextToken(self):
+        print("Lorem Ipsum")
+    def identifierExists(self, identifier):
+        print ("Lorem Ipsum")
+    def begin(self):
+        print("Lorem Ipsum")
+
